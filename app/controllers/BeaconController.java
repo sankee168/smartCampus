d3017eb99f096ade6eabbcad879adb2628308d7f
Sankeerth <sankee168@gmail.com>
2016-04-10 17:50:03 -0400

    added event view with iterating over events

--- app/controllers/BeaconController.java
+++ app/controllers/BeaconController.java
@@ -4,10 +4,16 @@ import com.avaje.ebean.Ebean;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import models.database.Beacon;
+import models.database.Event;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.twirl.api.Html;
+import scala.collection.JavaConverters;
+import views.html.event;
+
+import java.util.Iterator;
 
 /**
  * Created by mallem on 4/5/16.
@@ -22,7 +28,8 @@ public class BeaconController extends Controller {
 
     public Result getBeaconById(String id) {
         Beacon beacon = Ebean.find(Beacon.class).where().ieq("id", id).findUnique();
-        return ok(beacon.toString());
+//        scala.collection.immutable.List<String> ls = JavaConverters.asScalaBufferConverter(beacon.getEvents()).asScala();
+        return ok(event.render(beacon.getEvents()));
     }
 
     public Result createBeacon() {
